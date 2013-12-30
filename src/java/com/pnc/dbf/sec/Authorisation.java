35d45370392c3ae23fb71fc7cb37c4242e306a5c
Dedrick Kitamuka <kitamuka@gmail.com>
2013-12-31 00:34:37 +0100

    first commit, fixing

---
+++ src/java/com/pnc/dbf/sec/Authorisation.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pnc.dbf.sec;
+
+/**
+ *
+ * @author IMA7
+ */
+public class Authorisation {
+    
+}
